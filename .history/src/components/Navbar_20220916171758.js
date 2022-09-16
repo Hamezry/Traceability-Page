8f8ed4f32d19848ecec69a9a9acc70849c83feb0
James Okafor <jamesokafor@Jamess-MacBook-Pro-2.local>
2022-09-16 17:22:13 +0100

    another update

--- .history/src/components/Navbar_20220916171758.js
+++ .history/src/components/Navbar_20220916171758.js
@@ -6,7 +6,7 @@ const Navbar = () => {
     <div className='w-full flex justify-center h-[90px] bg-white sticky font-muli  z-30  top-0 lg:items-center lg:justify-between '>
         
         <img src= {logo} alt='nav-icon' className='h-[80px] w-[188px] lg:ml-[50px]'/>
-        <p className='hidden lg:block mr-[230px] xl:mr-[180px] 2xl:mr-[320px] text-[#47494E] font-semibold  text-[20px]'>Traceability Information</p>
+        <p className='hidden lg:block mr-[230px] xl:mr-[190px] 2xl:mr-[320px] text-[#47494E] font-semibold  text-[20px]'>Traceability Information</p>
 
     </div>
   )
